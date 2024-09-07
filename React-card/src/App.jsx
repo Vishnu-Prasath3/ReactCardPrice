c94e9a57b4da9b7243bdc7199f716b08acc07c05
VishnuPrasath <eeevishnuprasath@gmail.com>
2024-09-07 20:15:10 +0530

    everything added

---
+++ React-card/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className='app'>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      </div>
+      </>
+  )
+}
+
+export default App
---
+++ React-card/src/Components/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Card.css"
+export default function (){
+
+
+    return( 
+       <>
+        <div></div>
+    <div className="card">
+        <button type="button" class="btn btn-primary">button</button>
+    </div>
+    </>
+    )
+}
+        
+    
