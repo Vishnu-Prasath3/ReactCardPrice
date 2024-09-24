b8bef666d2a85659fb07659fee22fcdf25c71162
VishnuPrasath <eeevishnuprasath@gmail.com>
2024-09-24 11:48:31 +0530

    readytohost

--- React-card/src/App.jsx
+++ React-card/src/App.jsx
@@ -5,20 +5,17 @@ import Card from './Components/Card'
 function App() {
   
  
- const freeDetails=["single user","50 gbstorage","unlimited public projects","community access"];
+ const freDetails=["single user","50 gbstorage","unlimited public projects","community access"];
  const feeDetails=["unlimited private projects","dedicated phone support","free subdomain","monthly status reports"];
- 
- const freeDetail1=["5-users","50gb storage","unlimited public projects","community access","unlimited private projects","dedicated phone support"]
  const feeDetail1=["monthly Status Reports"];
- 
  const freeDetail2=["unlimited projects","50gb storage","unnlimited public projects","community access","unlimited public pro"]
  const freedetail3=["unlimited users","50gb storage","unlimited public projects","community projects","unlimited private projects","dedicated phone support","free subdomain","monthly status report"]
   return (
     <>
     <div className='app'>
-      <Card title="Free" subtitle="$0/Month" freeDetails={freeDetails} feeDetails={feeDetails}></Card>
-      <Card title="plus" subtitle="$9/Month" freDetails={freeDetail2} feeDetails={feeDetail1} ></Card>
-      <Card title="pro"  subtitle="$49/Month" freDetails={freedetail3}></Card>
+      <Card title="Free" subtitle="$0/Month" offer={freDetails} paid={feeDetails}></Card>
+      <Card title="plus" subtitle="$9/Month" offer={freeDetail2} paid={feeDetail1} ></Card>
+      <Card title="pro"  subtitle="$49/Month" offer={freedetail3}></Card>
       </div>
       </>
   )
--- React-card/src/Components/Card.jsx
+++ React-card/src/Components/Card.jsx
@@ -1,30 +1,32 @@
 import React from "react";
 import "./Card.css"
-export default function ({title,subtitle,freDetails=[],feeDetails=[]}){
+export default function ({title,subtitle,offer=[],paid=[]}){
 
     return( 
-
-       <>                     
+       <>
+       <div className="main-comp">                     
     <div className="card">
-    
     <div className="title1"> <p>{title}</p>
     <h1>{subtitle}</h1>
     <hr></hr>
     </div>
 <div>
 <ul className="fre-details">
-    
-{freDetails.map((e,index)=>(
+    {/* free things were added */}
+
+    {offer.map((e,index)=>(
         <li key={index}>{e}</li>
         ))
     } </ul>
+
+    {/* cost should be paid */}
     <ul className="free-details">
-    {feeDetails.map((e,index)=>(
+    {paid.map((e,index)=>(
             <li key={index}>{e}</li>  ))}
-</ul>
-            
+</ul>            
 </div>
         <button type="button" className="btn btn-primary">button</button>
+    </div>
     </div>
     </>
     )
